195002480dd90f085deff573a8f67dd57eb8275a
Justas <zdanaviciusjustas1@gmail.com>
2022-12-07 13:11:15 +0100

    Settings page

--- system/system.js
+++ system/system.js
@@ -1,12 +1,10 @@
 redirectUnauthenticatedUser();
 
-getOverview();
+getOverview(true);
 
 window.onload = () => {
     //Details
     document.querySelector(".main-logo").addEventListener('click', () => location.href = "/");
-
-    
 
     // Enable alerts
     const alerts = document.createElement("div");
@@ -39,7 +37,34 @@ function initChangePage(btnElement, functionality){
     })
 }
 
-function getOverview() {
+function initOverviewFunctionality(res){
+    //Handle user info
+    const user = JSON.parse(res).user;
+    const userNameElement = document.querySelector('#account-name');
+    userNameElement.innerText = user.name; // + ' ' + user.surname;
+
+    //Handle balance
+    const balance = JSON.parse(res).balance;
+
+    //Handle household data
+    const households = JSON.parse(res).households;
+    households.forEach(addHouseholdUI); //Add as elements
+
+    //Handle expenses and their data
+    const expenses = JSON.parse(res).expenses;
+
+    //Browsing logic
+    const overviewBtn = document.querySelector('#overview-link');
+    const settingsBtn = document.querySelector('#settings-link');
+    initChangePage(overviewBtn, getOverview);
+    initChangePage(settingsBtn, getSettings);
+
+    //Show overview by default
+    overviewBtn.classList.add('active');
+    document.querySelector('#overview').classList.add('active');
+}
+
+function getOverview(initializeFunctionality) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://45.80.152.150/overview', true);
     xhr.allowJson();
@@ -48,31 +73,8 @@ function getOverview() {
     xhr.setError();
     xhr.onload = function() {
         if((xhr.status === 200)){
-            
-            //Handle user info
-            const user = JSON.parse(xhr.response).user;
-            const userNameElement = document.querySelector('#account-name');
-            userNameElement.innerText = user.name + ' ' + user.surname;
-
-            //Handle balance
-            const balance = JSON.parse(xhr.response).balance;
-            
-            //Handle household data
-            const households = JSON.parse(xhr.response).households;
-            households.forEach(addHouseholdUI); //Add as elements
-            
-            //Handle expenses and their data
-            const expenses = JSON.parse(xhr.response).expenses;
-
-            //Browsing logic
-            const overviewBtn = document.querySelector('#overview-link');
-            const settingsBtn = document.querySelector('#settings-link');
-            // initChangePage(overviewBtn, getOverview);
-            initChangePage(settingsBtn, getSettings);
-
-            //Show overview by default
-            overviewBtn.classList.add('active');
-            document.querySelector('#overview').classList.add('active');
+            if(initializeFunctionality) 
+                initOverviewFunctionality(xhr.response);
         }
     };
     xhr.send();
@@ -95,7 +97,7 @@ function addHouseholdUI(household) {
     const pHouseholdElement = document.createElement('p');
     pHouseholdElement.dataset.id = household.hhid;
     pHouseholdElement.innerHTML = '<span class="material-icons icn">home</span> ' + household.name;
-    initChangePage(pHouseholdElement); //Click functionality
+    initChangePage(pHouseholdElement, () => {}); //Click functionality
 
     const ulHouseholdElement = document.createElement('ul');
     household.users.forEach((user) => {
@@ -118,13 +120,80 @@ function getSettings() {
     xhr.setError();
     xhr.onload = function() {
         if((xhr.status === 200)){
-            const resp = JSON.parse(xhr.response);
-            document.querySelector('#pi-email').value = resp.user.email;
-            document.querySelector('#pi-name').value = resp.user.name;
-            document.querySelector('#pi-surname').value = resp.user.surname;
+            const res = JSON.parse(xhr.response);
+            document.querySelector('#pi-email').value = res.user.email;
+            document.querySelector('#pi-name').value = res.user.name;
+            document.querySelector('#pi-surname').value = res.user.surname;
 
-            document.querySelector('#pi-income').value = resp.user.estimatedMonthlyIncome;
+            document.querySelector('#pi-income').value = res.estimatedMonthlyIncome;
+
+            const newsletterCheckbox = document.querySelector('#pi-newsletter');
+            newsletterCheckbox.addEventListener('click', () => {
+                if(newsletterCheckbox.dataset.checked == 'true'){
+                    newsletterCheckbox.classList.remove('active');
+                    newsletterCheckbox.dataset.checked = 'false';
+                }
+                else {
+                    newsletterCheckbox.classList.add('active');
+                    newsletterCheckbox.dataset.checked = 'true';
+                }
+            });
+            if(res.newsletter){
+                newsletterCheckbox.classList.add('active');
+                newsletterCheckbox.dataset.checked = 'true';
+            }
+            
+
+            const hhContainer = document.querySelector('#settings .hh-container');
+            while(hhContainer.firstChild) hhContainer.removeChild(hhContainer.firstChild);
+            res.households.forEach((hh) => {
+                const hhBlock = document.createElement('div');
+                hhBlock.innerHTML = `
+                    <h4 class="hh-name">${hh.name}</h4>
+                    <p class="hh-id">ID: ${hh.hhid}</p>
+                    <label for="Room size">Room size, m²</label>
+                    <input type="number" class="hh-room-size" value="${hh.roomSize}">
+                    <span class="material-icons hh-leave">remove_circle</span>
+                `;
+                hhBlock.classList.add('hh-block');
+                hhBlock.dataset.id = hh.hhid;
+                hhBlock.querySelector('.hh-room-size').disabled = !hh.canEdit;
+                if(!hh.canLeave) hhBlock.querySelector('.hh-leave').style.display = 'none';
+                hhBlock.querySelector('.hh-leave').addEventListener('click', () => {
+                    if(hh.canLeave) leaveHousehold(hh.hhid);
+                });
+                hhContainer.appendChild(hhBlock);
+            });
         }
     };
     xhr.send();
 }   
+
+function updatePersonalInfo(){
+    try {
+        //Gather JSON body
+        const json = {
+            
+        }
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('PUT', 'http://45.80.152.150/personal-info', true);
+        xhr.allowJson();
+        xhr.addToken();
+        xhr.setStandardTimeout();
+        xhr.setError();
+        xhr.onload = function() {
+            if((xhr.status === 200)){
+                inform(xhr.statusText, "success");
+            }
+            else {
+                inform(xhr.statusText, "failure");
+            }
+        };
+        xhr.send(json);
+    } catch(err) {
+        return false;
+    }
+
+    return false;
+}
